fdd700a76857739968457d9bcc2883627db8d757
ShreyasR1718 <rshreyas1718@gmail.com>
2026-09-19 16:53:01 +0530

    Fix database path

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,16 @@
 import sqlite3
+import os
 
-DATABASE_NAME = "kairo.db"
+DATABASE_NAME = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "kairo.db"
+)
 
 
 def get_connection():
+
     connection = sqlite3.connect(DATABASE_NAME)
+
     connection.row_factory = sqlite3.Row
+
     return connection
